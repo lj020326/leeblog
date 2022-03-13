00f4b94cb2eca30796c056bb3fb5087c26a3ea56
LJ Johnson <lj020326@gmail.com>
2022-03-13 18:19:12 -0400

    updates

--- pelicanconf.py
+++ pelicanconf.py
@@ -37,12 +37,15 @@ PLUGINS = [
     'summary',       # auto-summarizing articles
     'feed_summary',  # use summaries for RSS, not full articles
     'ipynb.liquid',  # for embedding notebooks
-    'liquid_tags.img',  # embedding images
-    'liquid_tags.video',  # embedding videos
-    'liquid_tags.include_code',  # including code blocks
-    'liquid_tags.literal'
+    # 'liquid_tags.img',  # embedding images
+    # 'liquid_tags.video',  # embedding videos
+    # 'liquid_tags.include_code',  # including code blocks
+    # 'liquid_tags.literal'
 ]
 IGNORE_FILES = ['.ipynb_checkpoints']
+
+LIQUID_TAGS = ["img", "literal", "video", "youtube",
+               "vimeo", "include_code"]
 
 # for liquid tags
 CODE_DIR = 'downloads/code'
@@ -57,8 +60,8 @@ GITHUB_USERNAME = 'lj020326'
 LINKEDIN_USERNAME = 'leejjohnson'
 STACKOVERFLOW_ADDRESS = 'https://stackexchange.com/users/3319937/purplemouse'
 AUTHOR_WEBSITE = 'https://epgtec.com'
-AUTHOR_BLOG = 'http://lj020326.github.io'
-AUTHOR_CV = "http://lj020326.github.io/downloads/docs/CV.pdf"
+AUTHOR_BLOG = 'http://leeblog.org'
+AUTHOR_CV = "http://leeblog.org/downloads/docs/CV.pdf"
 SHOW_ARCHIVES = True
 SHOW_FEED = False  # Need to address large feeds
 
@@ -68,5 +71,5 @@ STATIC_PATHS = ['images', 'figures', 'videos', 'downloads', 'favicon.ico']
 
 # Footer info
 
-LICENSE_URL = "https://github.com/lj020326/lj020326.github.io-source/blob/master/LICENSE"
+LICENSE_URL = "https://github.com/lj020326/leeblog.org/blob/master/LICENSE"
 LICENSE = "MIT"
