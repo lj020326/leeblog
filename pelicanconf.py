33a7179375b53dc8aea8b5ffb21a928984af46e6
Lee Johnson <lee.james.johnson@gmail.com>
2022-06-13 08:37:01 -0400

    src - 	modified:   pelicanconf.py Changes not staged for commit:   (use "git add <file>..." to update what will be committed)   (commit or discard the untracked or modified content in submodules) 	modified:   plugins/pelican-plugins (modified content)

--- pelicanconf.py
+++ pelicanconf.py
@@ -27,9 +27,9 @@ DEFAULT_PAGINATION = 10
 #RELATIVE_URLS = True
 
 MARKUP = ["md", "ipynb"]
-# PLUGIN_PATHS = ['./plugins', './plugins/pelican-plugins']
+PLUGIN_PATHS = ['./plugins', './plugins/pelican-plugins']
 # PLUGIN_PATHS = ['./plugins', './pelican-plugins']
-PLUGIN_PATHS = ['./pelican-plugins']
+# PLUGIN_PATHS = ['./pelican-plugins']
 PLUGINS = [
     'summary',       # auto-summarizing articles
     'liquid_tags',   # for notebooks
